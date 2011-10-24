b330125911ac55707d5fc1aa017b31769ac1b55a
candide <candide@92e5ae4a-8afd-0310-8b33-eaebc6dd6768>
2011-10-24 23:56:03 +0000

    - First draft template script for SQL.
    git-svn-id: file:///home/candide/src/0_Local_SVN_Repos/templates/trunk@2292 92e5ae4a-8afd-0310-8b33-eaebc6dd6768

---
+++ script.sql
@@ -0,0 +1,27 @@
+-- -*- Sql -*-
+--######################################################################
+--
+-- Copyright (C) 2011 by John P. Weiss
+--
+-- This package is free software; you can redistribute it and/or modify
+-- it under the terms of the Artistic License, included as the file
+-- "LICENSE" in the source code archive.
+--
+-- This package is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+--
+-- You should have received a copy of the file "LICENSE", containing
+-- the License John Weiss originally placed this program under.
+--
+-- $Id: script.sh 2087 2010-12-30 22:58:59Z candide $
+--######################################################################
+
+
+
+
+
+--################################
+-- Local Variables:
+-- sql-product: ansi
+-- End:
